92fc8f0fda333e8cd8c8861bb386f191ab66f990
Cosmin-Ioan Grigoruta <grigorutacosmin@gmail.com>
2022-01-30 22:23:05 +0200

    Implement User UI for auth/reg

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  model: any = {};
+
+  constructor(private http: HttpClient, public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.model = {}
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  fields : any [] = [{
+    name: 'username',
+    type: 'text',
+    label: 'Username'
+  },
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Password'
+  },
+  {
+    name: 'firstname',
+    type: 'text',
+    label: 'First Name'
+  },
+  {
+    name: 'lastname',
+    type: 'text',
+    label: 'Last Name'
+  },
+  {
+    name: 'email',
+    type: 'text',
+    label: 'Email'
+  },
+  {
+    name: 'country',
+    type: 'text',
+    label: 'Country'
+  }]
+  model: any = {};
+  constructor(public accountService: AccountService) { }
+  ngOnInit(): void {
+  }
+
+  isFormDisabled() {
+    return !this.model.username || !this.model.password || !this.model.firstname || !this.model.lastname || !this.model.country || !this.model.type;
+  }
+
+  register() {
+    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+}
