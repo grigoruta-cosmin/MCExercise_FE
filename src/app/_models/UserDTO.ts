aa8b2ecffc9d036f9184c7dc688b3d435d977e8e
Cosmin-Ioan Grigoruta <grigorutacosmin@gmail.com>
2022-01-31 23:50:45 +0200

    Implement DTO

---
+++ src/app/_models/UserDTO.ts
@@ -0,0 +1,11 @@
+import { PhotoDTO } from "./PhotoDTO";
+
+export interface UserDTO {
+    firstName: string;
+    lastName: string;
+    email: string;
+    country: string;
+    bio: string;
+    usermame: string;
+    type: string;
+}
